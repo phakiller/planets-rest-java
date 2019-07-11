835e189e4a8277e8a4c9ef1ae645c4ec0c15c36d
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2019-07-10 21:11:03 -0300

    Create 'savePlanet' method to save a planet in 'PlanetApplicationService' class.

--- src/main/java/com/starwars/planets/application/planet/PlanetApplicationService.java
+++ src/main/java/com/starwars/planets/application/planet/PlanetApplicationService.java
@@ -1,5 +1,10 @@
 package com.starwars.planets.application.planet;
 
+import com.starwars.planets.application.planet.dto.PlanetCommand;
+import com.starwars.planets.application.planet.dto.PlanetDTO;
+import com.starwars.planets.application.planet.factory.PlanetCommandFactory;
+import com.starwars.planets.application.planet.factory.PlanetFactory;
+import com.starwars.planets.domain.model.planet.Planet;
 import com.starwars.planets.domain.service.planet.PlanetService;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +15,11 @@ public class PlanetApplicationService {
 
     public PlanetApplicationService(PlanetService planetService) {
         this.planetService = planetService;
+    }
+
+    public PlanetDTO savePlanet(PlanetCommand planetCommand) {
+        Planet planet = planetService.save(PlanetCommandFactory.toPlanet(planetCommand));
+        return PlanetFactory.toPlanetDTO(planet);
     }
 
 }
