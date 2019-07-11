b2f5445057ffc9d01754b033c2a1fe58f54cfcf9
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2019-07-10 21:03:21 -0300

    Define 'Planet' domain model.

--- src/main/java/com/starwars/planets/domain/model/planet/Planet.java
+++ src/main/java/com/starwars/planets/domain/model/planet/Planet.java
@@ -1,4 +1,23 @@
 package com.starwars.planets.domain.model.planet;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor(onConstructor_ = @Builder)
+@Document(collection = "planet")
 public class Planet {
+
+    @Id
+    private String id;
+    private String name;
+    private String climate;
+    private String terrain;
+
 }
