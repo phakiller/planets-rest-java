760610c72735fd76ff31f114413dedeb3fb8c48d
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2019-07-10 21:05:41 -0300

    Create save method on domain layer to Planet model.

--- src/main/java/com/starwars/planets/domain/service/planet/PlanetDomainService.java
+++ src/main/java/com/starwars/planets/domain/service/planet/PlanetDomainService.java
@@ -1,7 +1,20 @@
 package com.starwars.planets.domain.service.planet;
 
+import com.starwars.planets.domain.model.planet.Planet;
+import com.starwars.planets.port.adapters.repositories.planet.PlanetRepositoryMongoDB;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PlanetDomainService implements PlanetService {
+
+    private final PlanetRepositoryMongoDB planetRepositoryMongoDB;
+
+    public PlanetDomainService(PlanetRepositoryMongoDB planetRepositoryMongoDB) {
+        this.planetRepositoryMongoDB = planetRepositoryMongoDB;
+    }
+
+    public Planet save(Planet planet) {
+        return planetRepositoryMongoDB.save(planet);
+    }
+
 }
--- src/main/java/com/starwars/planets/domain/service/planet/PlanetService.java
+++ src/main/java/com/starwars/planets/domain/service/planet/PlanetService.java
@@ -1,4 +1,9 @@
 package com.starwars.planets.domain.service.planet;
 
+import com.starwars.planets.domain.model.planet.Planet;
+
 public interface PlanetService {
+
+    Planet save(Planet planet);
+
 }
