9104a56ab541aed5610ec2e0a349c8d2d970c31e
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2019-07-10 21:32:18 -0300

    Add 'findAll' method to 'PlanetRepository' interface.

--- src/main/java/com/starwars/planets/domain/service/planet/PlanetRepository.java
+++ src/main/java/com/starwars/planets/domain/service/planet/PlanetRepository.java
@@ -2,8 +2,12 @@ package com.starwars.planets.domain.service.planet;
 
 import com.starwars.planets.domain.model.planet.Planet;
 
+import java.util.List;
+
 public interface PlanetRepository {
 
     Planet save(Planet planet);
+
+    List<Planet> findAll();
 
 }
