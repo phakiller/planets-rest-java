bc5a0dc584eb89769518a0f7d51573ca39070b19
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2019-07-10 21:08:45 -0300

    Create 'PlanetDTO' class.

---
+++ src/main/java/com/starwars/planets/application/planet/dto/PlanetDTO.java
@@ -0,0 +1,21 @@
+package com.starwars.planets.application.planet.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor(onConstructor_ = @Builder)
+public class PlanetDTO implements Serializable {
+
+    private String id;
+    private String name;
+    private String climate;
+    private String terrain;
+
+}
