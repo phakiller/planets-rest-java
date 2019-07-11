8d2cc4fa9e0516b71b550313cb8569f9e6522ff4
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2019-07-10 21:34:50 -0300

    Expose route to retrive all saved planets, (GET) '/planets'.

--- src/main/java/com/starwars/planets/port/adapters/rest/PlanetController.java
+++ src/main/java/com/starwars/planets/port/adapters/rest/PlanetController.java
@@ -4,12 +4,12 @@ import com.starwars.planets.application.planet.PlanetApplicationService;
 import com.starwars.planets.application.planet.dto.PlanetCommand;
 import com.starwars.planets.application.planet.dto.PlanetDTO;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.ResponseEntity.ok;
 import static org.springframework.http.ResponseEntity.status;
 
 @RestController
@@ -26,6 +26,11 @@ public class PlanetController {
     public ResponseEntity<PlanetDTO> savePlanet(@RequestBody PlanetCommand planetCommand) {
         PlanetDTO planetDTO = planetApplicationService.savePlanet(planetCommand);
         return status(CREATED).body(planetDTO);
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<PlanetDTO>> getPlanets() {
+        return ok().body(planetApplicationService.getPlanets());
     }
 
 }
