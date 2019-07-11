314f10463bcd8f220ed3231008b03c9b4b2011a4
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2019-07-10 21:14:42 -0300

    Expose route to save a planet, (POST) '/planets', in 'PlanetController' class.

--- src/main/java/com/starwars/planets/port/adapters/rest/PlanetController.java
+++ src/main/java/com/starwars/planets/port/adapters/rest/PlanetController.java
@@ -1,9 +1,31 @@
 package com.starwars.planets.port.adapters.rest;
 
+import com.starwars.planets.application.planet.PlanetApplicationService;
+import com.starwars.planets.application.planet.dto.PlanetCommand;
+import com.starwars.planets.application.planet.dto.PlanetDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.ResponseEntity.status;
 
 @RestController
 @RequestMapping("/planets")
 public class PlanetController {
+
+    private final PlanetApplicationService planetApplicationService;
+
+    public PlanetController(PlanetApplicationService planetApplicationService) {
+        this.planetApplicationService = planetApplicationService;
+    }
+
+    @PostMapping("")
+    public ResponseEntity<PlanetDTO> savePlanet(@RequestBody PlanetCommand planetCommand) {
+        PlanetDTO planetDTO = planetApplicationService.savePlanet(planetCommand);
+        return status(CREATED).body(planetDTO);
+    }
+
 }
