343887da860970e9ea80f1f29e542ea993b99606
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2019-07-10 21:31:35 -0300

    Add 'getPlanets' method to domain service layer.

--- src/main/java/com/starwars/planets/domain/service/planet/PlanetDomainService.java
+++ src/main/java/com/starwars/planets/domain/service/planet/PlanetDomainService.java
@@ -4,6 +4,8 @@ import com.starwars.planets.domain.model.planet.Planet;
 import com.starwars.planets.port.adapters.repositories.planet.PlanetRepositoryMongoDB;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PlanetDomainService implements PlanetService {
 
@@ -15,6 +17,10 @@ public class PlanetDomainService implements PlanetService {
 
     public Planet save(Planet planet) {
         return planetRepositoryMongoDB.save(planet);
+    }
+
+    public List<Planet> getPlanets() {
+        return planetRepositoryMongoDB.findAll();
     }
 
 }
--- src/main/java/com/starwars/planets/domain/service/planet/PlanetService.java
+++ src/main/java/com/starwars/planets/domain/service/planet/PlanetService.java
@@ -2,8 +2,12 @@ package com.starwars.planets.domain.service.planet;
 
 import com.starwars.planets.domain.model.planet.Planet;
 
+import java.util.List;
+
 public interface PlanetService {
 
     Planet save(Planet planet);
+
+    List<Planet> getPlanets();
 
 }
