9022b9fff76e9c8ce32abb61451aa6cb01dba25b
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2019-07-13 20:22:20 -0300

    Set number of films when a Planet model will be saved.

--- src/main/java/com/starwars/planets/domain/service/planet/PlanetDomainService.java
+++ src/main/java/com/starwars/planets/domain/service/planet/PlanetDomainService.java
@@ -2,6 +2,9 @@ package com.starwars.planets.domain.service.planet;
 
 import com.starwars.planets.domain.exceptions.NotFoundException;
 import com.starwars.planets.domain.model.planet.Planet;
+import com.starwars.planets.domain.model.swapi.SwapiPlanet;
+import com.starwars.planets.domain.model.swapi.SwapiPlanetPage;
+import com.starwars.planets.domain.service.swapi.SwapiService;
 import com.starwars.planets.port.adapters.repositories.planet.PlanetRepositoryMongoDB;
 import org.springframework.stereotype.Service;
 
@@ -12,12 +15,21 @@ import java.util.Optional;
 public class PlanetDomainService implements PlanetService {
 
     private final PlanetRepositoryMongoDB planetRepositoryMongoDB;
+    private final SwapiService swapiService;
 
-    public PlanetDomainService(PlanetRepositoryMongoDB planetRepositoryMongoDB) {
+    public PlanetDomainService(
+            PlanetRepositoryMongoDB planetRepositoryMongoDB,
+            SwapiService swapiService) {
         this.planetRepositoryMongoDB = planetRepositoryMongoDB;
+        this.swapiService = swapiService;
     }
 
     public Planet save(Planet planet) {
+        SwapiPlanetPage swapiPlanetPage = swapiService.findByName(planet.getName());
+
+        if (swapiPlanetPage != null)
+            setNumberOfFilms(planet, swapiPlanetPage);
+
         return planetRepositoryMongoDB.save(planet);
     }
 
@@ -36,6 +48,19 @@ public class PlanetDomainService implements PlanetService {
 
     public void deleteById(String planetId) {
         planetRepositoryMongoDB.deleteById(planetId);
+    }
+
+    private void setNumberOfFilms(Planet planet, SwapiPlanetPage swapiPlanetPage) {
+        SwapiPlanet swapiPlanet = swapiPlanetPage.getResults().get(0);
+
+        if (swapiPlanetPage.getCount().equals(1) && isPlanetsNamesEqualsIgnoringCase(planet, swapiPlanet))
+            planet.setNumberOfMovies(swapiPlanet.getFilms().size());
+        else
+            planet.setNumberOfMovies(-1);
+    }
+
+    private Boolean isPlanetsNamesEqualsIgnoringCase (Planet planet, SwapiPlanet swapiPlanet) {
+        return swapiPlanet.getName().equalsIgnoreCase(planet.getName());
     }
 
 }
