8d7ff8b0c2fb8fa95413b7e01975410222df5da4
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2019-07-13 18:25:53 -0300

    Crate 'findByNameMatchesRegexAndIgnoreCase' method to 'PlanetRepositorySpringData' interface.

--- src/main/java/com/starwars/planets/port/adapters/repositories/planet/PlanetRepositorySpringData.java
+++ src/main/java/com/starwars/planets/port/adapters/repositories/planet/PlanetRepositorySpringData.java
@@ -2,8 +2,15 @@ package com.starwars.planets.port.adapters.repositories.planet;
 
 import com.starwars.planets.domain.model.planet.Planet;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PlanetRepositorySpringData extends MongoRepository<Planet, String> {
+
+    @Query("{ 'name': { '$regex': '?0', '$options': 'i' } }")
+    List<Planet> findByNameMatchesRegexAndIgnoreCase(String name);
+
 }
